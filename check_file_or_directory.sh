0b7700937dcdec1b389906ab533ec42f42debda4
47259_Shagun <chshagun72@gmail.com>
2021-09-27 19:42:28 +0530

    script basics

--- check_file_or_directory.sh
+++ check_file_or_directory.sh
@@ -7,13 +7,11 @@ read -p "Enter path : " path
 if [[ -f "$path" ]]
 then
 	echo "It is a File"
-else
+elif [[ -d "path" ]] 
+then
 	echo "It is a Directory"
+else
+	echo "Invalid Path"
 fi
 
-#if [[ -d "$path" ]]
-#then
-#	echo "It is a Directory"
-#else
-#	echo "It is a File"
-#fi
+
